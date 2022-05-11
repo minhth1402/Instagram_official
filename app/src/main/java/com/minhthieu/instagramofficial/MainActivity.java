b51c46156ca54130c986fd6f18a7e59f6fb1adcb
minhthieu <minhthieu185@gmail.com>
2022-05-11 07:21:37 +0700

    Initial commit

---
+++ app/src/main/java/com/minhthieu/instagramofficial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.minhthieu.instagramofficial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
