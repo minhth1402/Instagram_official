15742f2328be9d8d2ae7175b43081a1e7c11a2c3
minhthieu <minhthieu185@gmail.com>
2022-05-12 15:36:35 +0700

    Horizontal scroll view in Home tab (but looking a bit better)

--- app/src/main/java/com/minhthieu/instagramofficial/Fragments/HomeFragment.java
+++ app/src/main/java/com/minhthieu/instagramofficial/Fragments/HomeFragment.java
@@ -13,6 +13,8 @@ import com.minhthieu.instagramofficial.Adapters.HomeRecyclerItemAdapter;
 import com.minhthieu.instagramofficial.Models.HomeRecyclerItem;
 import com.minhthieu.instagramofficial.R;
 import com.yarolegovich.discretescrollview.DiscreteScrollView;
+import com.yarolegovich.discretescrollview.InfiniteScrollAdapter;
+import com.yarolegovich.discretescrollview.transform.ScaleTransformer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +32,30 @@ public class HomeFragment extends Fragment {
         // Anh xa
         horizontalScroller = view.findViewById(R.id.horizontal_scroller);
 
+        // add them item vao list
         AddItemToHomeCard();
 
-        // set Adapter
-        horizontalScroller.setAdapter(new HomeRecyclerItemAdapter(homeRecyclerItems));
+        // do some scrollview setting
 
+        // the infinite scroll
+        InfiniteScrollAdapter<?> infiniteScrollAdapter = InfiniteScrollAdapter.wrap(new HomeRecyclerItemAdapter(homeRecyclerItems));
+
+        // set Adapter
+        horizontalScroller.setAdapter(infiniteScrollAdapter);
+
+        // scroller settings
+        horizontalScroller.setSlideOnFling(true);
+        horizontalScroller.setSlideOnFlingThreshold(1200);
+        horizontalScroller.setItemTransitionTimeMillis(400);
+
+        // set the zoom out / focus effect
+        horizontalScroller.setItemTransformer(new ScaleTransformer.Builder()
+                .setMinScale(0.8f)
+                .build());
+
+
+        // this belong to the fragment
+        // cai ly do ma no phai dung view như vầy là vì cái minh phải khai cái view trên kia mà tìm findViewById (vì có nhiều fragment mà)
         return view;
     }
 
