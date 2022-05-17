840dda47ccc9dea2599b453e160614990dfcdac9
minhthieu <minhthieu185@gmail.com>
2022-05-17 15:09:17 +0700

    Add like function to home

--- app/src/main/java/com/minhthieu/instagramofficial/Adapters/PostItemAdapter.java
+++ app/src/main/java/com/minhthieu/instagramofficial/Adapters/PostItemAdapter.java
@@ -16,6 +16,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.minhthieu.instagramofficial.Models.PostItem;
@@ -55,7 +56,6 @@ public class PostItemAdapter extends RecyclerView.Adapter<PostItemAdapter.PostIt
     @Override
     public void onBindViewHolder(@NonNull @NotNull PostItemAdapter.PostItemViewHolder holder, int position) {
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
 
         PostItem post = postList.get(position);
 
@@ -70,7 +70,7 @@ public class PostItemAdapter extends RecyclerView.Adapter<PostItemAdapter.PostIt
 
                         holder.fullName.setText(userInfo.getFullname());
 
-                        holder.profileImage.setImageURI(Uri.parse(userInfo.getImageUrl()) );
+                        holder.profileImage.setImageURI(Uri.parse(userInfo.getImageUrl()));
                     }
 
                     @Override
@@ -78,11 +78,72 @@ public class PostItemAdapter extends RecyclerView.Adapter<PostItemAdapter.PostIt
 
                     }
                 });
+
+        // ContainOnClickListener
+        ContainOnClickListener(holder, post);
+
+    }
+
+    private void ContainOnClickListener(PostItemAdapter.PostItemViewHolder holder, PostItem post) {
+        holder.likeBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isLiked(post.getPostId(),holder.likeBt);
+                LikesCounter(post.getPostId(), holder.likeCount);
+
+                if (holder.likeBt.getTag()=="like") {
+                    FirebaseDatabase.getInstance().getReference("Likes").child(post.getPostId()).child(user.getUid())
+                            .setValue(true);
+                } else {
+                    FirebaseDatabase.getInstance().getReference("Likes").child(post.getPostId()).child(user.getUid())
+                            .setValue(null);
+
+                }
+            }
+        });
+
+
+    }
+
+    private void isLiked(String postId, ImageView likeBt) {
+        FirebaseDatabase.getInstance().getReference("Likes").child(postId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                if(snapshot.child(user.getUid()).exists()){
+                    likeBt.setImageResource(R.drawable.ic_baseline_favorite_24);
+                    likeBt.setTag("liked");
+                } else{
+                    likeBt.setImageResource(R.drawable.ic_baseline_favorite_border_24);
+                    likeBt.setTag("like");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
         return postList.size();
+    }
+
+
+    void LikesCounter(String postId, TextView likeCount){
+        FirebaseDatabase.getInstance().getReference("Likes").child(postId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                likeCount.setText(String.valueOf(snapshot.getChildrenCount()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
     }
 
     public class PostItemViewHolder extends RecyclerView.ViewHolder {
@@ -104,7 +165,13 @@ public class PostItemAdapter extends RecyclerView.Adapter<PostItemAdapter.PostIt
             likeCount = itemView.findViewById(R.id.like_count);
             postImage = itemView.findViewById(R.id.post_image);
 
+            // firebase
+            user = FirebaseAuth.getInstance().getCurrentUser();
+
+
         }
     }
+
+
 
 }
